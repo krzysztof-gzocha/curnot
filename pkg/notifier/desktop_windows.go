b70ccba8017f69e4ec08152333601cce381e3bac
Krzysztof Gzocha <kgzocha@gmail.com>
2022-12-27 20:54:19 +0100

    Fix Dekstop notifier on windows

--- pkg/notifier/desktop_windows.go
+++ pkg/notifier/desktop_windows.go
@@ -3,6 +3,8 @@
 package notifier
 
 import (
+	"context"
+
 	"github.com/go-toast/toast"
 	"github.com/krzysztof-gzocha/curnot/pkg/aggregator"
 )
@@ -13,7 +15,7 @@ func NewDesktop() *Desktop {
 	return &Desktop{}
 }
 
-func (d *Desktop) Notify(msg aggregator.RateChange) error {
+func (d *Desktop) Notify(ctx context.Context, msg aggregator.RateChange) error {
 	notification := toast.Notification{
 		// https://github.com/go-toast/toast/issues/9
 		AppID:   "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe",
